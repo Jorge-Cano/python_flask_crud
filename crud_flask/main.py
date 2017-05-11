704cf936569b0991cf4e4e9794adb1768737e30b
Jorge Cano <jacinaustin@gmail.com>
2017-05-11 15:11:59 -0500

    commits

--- crud_flask/main.py
+++ crud_flask/main.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jorgecano:123@localhost/wdiflask'
 
